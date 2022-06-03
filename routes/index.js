b9f103e06b240a2b077ffd732a048d78ca3f247f
Matthew Mikhaiel <MattDFD@live.ca>
2022-06-03 14:38:25 +0200

    added second view.01

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Home' });
+  res.render('index', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET products page. */
